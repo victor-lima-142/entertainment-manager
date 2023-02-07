805e82fc6083e846eb7369d68a5af45a42a33ce5
Victor Reboredo <victor.reboredo@ipremios.com>
2023-02-07 15:03:42 -0300

    Feature: Adding seeder of Series.

--- api/database/seeders/SerieSeeder.php
+++ api/database/seeders/SerieSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Serie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,23 @@ class SerieSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $series = [
+            "The Last Of Us" => "2023-01-15 00:00:00",
+            "Grey's Anatomy" => "2005-03-27 00:00:00",
+            "Breaking Bad - A Química do Mal" => "2013-09-29 00:00:00",
+            "Rick and Morty" => "2013-12-02 00:00:00",
+            "The Last Kingdom" => "2015-10-10 00:00:00"
+        ];
+        foreach ($series as $name => $release) {
+            try {
+                $serie = new Serie([
+                    'name' => $name,
+                    'release' => $release
+                ]);
+                $serie->save();
+            } catch (\Exception $e) {
+                throw $e;
+            }
+        }
     }
 }
