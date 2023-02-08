1e2422761010b38bf9d344f1298962277fa3c9f8
Victor Reboredo <victor.reboredo@ipremios.com>
2023-02-08 10:48:09 -0300

    Refactor: Changes in serie card

--- web-platform/src/components/series/list/index.tsx
+++ web-platform/src/components/series/list/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Card, Carousel, Container, Row } from "react-bootstrap";
+import "./list.scss";
 
 const SeriesList = (props: SeriesData): JSX.Element => {
     const { list } = props;
 
     return <div className='list-series-container'>
             {list.map((serie: any, index: any) => {
-                const { name, release } = serie;
+                const { name, id, image } = serie;
                 return <Card style={{ cursor: 'pointer' }} className="shadow-sm p-0 rounded-3 card-serie border-0" key={index}>
                     <Carousel variant="dark" controls={false} indicators={false} className="rounded-3 shadow-sm border-0">
                         <Carousel.Item className="rounded-3 border-0">
                             <img className="d-block w-100 shadow-sm rounded-3 border-0"
-                                src="https://mixdeseries.com.br/wp-content/uploads/2022/11/wandinha-melhores-series-netflix.webp" alt={`serie-${name}-${release}}`} />
+                                src={image} alt={`serie-${name}-${id}}`} />
                             <Carousel.Caption>
                                 <h5>{name}</h5>
                             </Carousel.Caption>
